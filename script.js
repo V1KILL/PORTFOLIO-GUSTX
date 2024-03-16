167df072b804a875634a01596b90f91481b7130a
Victor Amaral Mendonça <canalv1kill@gmail.com>
2024-03-16 16:57:23 -0300

    Zoom na Imagem
    Criei uma função que ao você clicar em uma das imagens do projeto, ela ficará em tela cheia junto com a descrição dela abaixo usando sweetalert2

---
+++ script.js
@@ -0,0 +1,7 @@
+function zoom() {
+    swal({
+        title: "Projeto Ben10",
+        icon: "ui.webp",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quidem excepturi amet autem aspernatur laudantium possimus dolorem beatae, dolores, eos est reprehenderit dicta sequi modi vitae consequuntur. Cupiditate, vitae qui.",
+    });
+}
