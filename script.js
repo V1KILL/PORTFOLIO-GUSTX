c541425a6614ef3bae7cc60623c97943a7cd7953
Victor Amaral Mendonça <canalv1kill@gmail.com>
2024-04-24 14:58:35 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function zoom(src) {
+function zoom(src, description) {
     swal({
         
         icon: `${src}`,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quidem excepturi amet autem aspernatur laudantium possimus dolorem beatae, dolores, eos est reprehenderit dicta sequi modi vitae consequuntur. Cupiditate, vitae qui.",
+        text: `${description}`,
     });
 }
